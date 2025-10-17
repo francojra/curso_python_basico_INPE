3b9521312b3cc64a1468f31d31ebf0612afd5c78
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-10-17 09:44:27 -0300

    Add files via upload

--- aula_manipulando_arquivos.py
+++ aula_manipulando_arquivos.py
@@ -19,3 +19,17 @@ x = arq.read()
 arq.close()
 
 print(x)
+
+## Acesso direto a arquivos
+
+fn = input("Entre com o nome do arquivo: ") 
+ 
+try: 
+    f= open(fn, "r") 
+    all = f.readlines() # Para ler todas as linhas
+    f.close() 
+ 
+    for line in all: # Para cada linha apresente o conteúdo dela
+        print(line) 
+except IOError as e: 
+    print("error:", e) 
